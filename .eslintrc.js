685c971446d62b03dabfa90f85d29beee00e8c2b
smc1040937705 <76459263+smc1040937705@users.noreply.github.com>
2025-09-17 16:43:02 +0800

    feat: add ESLint configuration for code quality checks

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-unused-vars': 'error',
+    'no-console': 'warn',
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single'],
+    'indent': ['error', 2],
+    'comma-dangle': ['error', 'always-multiline'],
+  },
+};
